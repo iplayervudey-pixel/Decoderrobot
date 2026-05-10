2377b68a07364ad847923620e02080568476bbf6
iplayervudey-pixel <iplayervudey@gmail.com>
2026-05-10 22:28:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,20 @@
-from pyrogram import Client, filters
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-
-FORCE_CHANNEL = int(os.getenv("FORCE_CHANNEL"))
-
-app = Client(
-    "bot",
-    bot_token=BOT_TOKEN,
-    api_id=API_ID,
-    api_hash=API_HASH
-)
-
-
 @app.on_message(filters.command("start"))
 async def start(client, message):
-    await message.reply_text("Bot Online")
 
-
-@app.on_message(filters.command("id"))
-async def idtest(client, message):
+    user_id = message.from_user.id
 
     try:
-
-        chat = await client.get_chat(FORCE_CHANNEL)
-
-        await message.reply_text(
-            f"CHANNEL ID:\\n{chat.id}"
+        member = await client.get_chat_member(
+            FORCE_CHANNEL,
+            user_id
         )
 
-    except Exception as e:
+        if member.status in ["member", "administrator", "owner"]:
+            await message.reply_text("Welcome")
+        else:
+            await message.reply_text("Join channel dulu")
 
+    except:
         await message.reply_text(
-            f"ERROR:\\n{e}"
+            "Silahkan join channel dulu"
         )
-
-
-app.run()
