28a34f0733af77ed0b1afffcf764e7134aee04cd
iplayervudey-pixel <iplayervudey@gmail.com>
2026-05-11 01:02:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 FORCE_CHANNEL = os.getenv("FORCE_CHANNEL")
-DB_CHANNEL = int(os.getenv("DB_CHANNEL"))
+DB_CHANNEL = os.getenv("DB_CHANNEL")
 
 app = Client(
     "memory_bot",
