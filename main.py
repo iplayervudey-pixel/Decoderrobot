35833982a1934999a2b222e1cec177d310801112
iplayervudey-pixel <iplayervudey@gmail.com>
2026-05-10 22:43:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,81 @@
 from pyrogram import Client, filters
+from pyrogram.errors import UserNotParticipant, FloodWait
+import asyncio
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 
-FORCE_CHANNEL = os.getenv("FORCE_CHANNEL")
+# contoh:
+# FORCE_CHANNEL=-1001234567890
+FORCE_CHANNEL = int(os.getenv("FORCE_CHANNEL"))
 
 app = Client(
-    "bot",
+    "memory_bot",
     bot_token=BOT_TOKEN,
     api_id=API_ID,
-    api_hash=API_HASH
+    api_hash=API_HASH,
+    in_memory=True
 )
 
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
-    await message.reply_text("Bot Online")
 
-
-@app.on_message(filters.command("id"))
-async def idtest(client, message):
+    user_id = message.from_user.id
 
     try:
 
-        chat = await client.get_chat(FORCE_CHANNEL)
-
-        await message.reply_text(
-            f"CHANNEL ID:\n{chat.id}"
+        member = await client.get_chat_member(
+            FORCE_CHANNEL,
+            user_id
         )
 
+        if member.status in [
+            "member",
+            "administrator",
+            "owner",
+            "restricted"
+        ]:
+
+            await message.reply_text(
+                "✅ Welcome To Bot"
+            )
+
+        else:
+
+            await message.reply_text(
+                "❌ Join channel dulu"
+            )
+
+    except UserNotParticipant:
+
+        await message.reply_text(
+            "❌ Kamu belum join channel"
+        )
+
+    except FloodWait as e:
+
+        print(f"FloodWait: {e.value}")
+
+        await asyncio.sleep(e.value)
+
     except Exception as e:
+
+        print(e)
 
         await message.reply_text(
             f"ERROR:\n{e}"
         )
+
+
+@app.on_message(filters.command("id"))
+async def getid(client, message):
+
+    await message.reply_text(
+        f"USER ID:\n{message.from_user.id}"
+    )
 
 
 app.run()
