78efe46a240e9dcd45c545cfa0073cc44a22e414
iplayervudey-pixel <iplayervudey@gmail.com>
2026-05-10 22:18:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 from pyrogram import Client, filters
 import os
 
-app = Client(
-    "bot",
-    bot_token=os.getenv("BOT_TOKEN"),
-    api_id=int(os.getenv("API_ID")),
-    api_hash=os.getenv("API_HASH")
-)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
 
 FORCE_CHANNEL = int(os.getenv("FORCE_CHANNEL"))
 
+app = Client(
+    "bot",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH
+)
+
+
 @app.on_message(filters.command("start"))
 async def start(client, message):
+    await message.reply_text("Bot Online")
+
+
+@app.on_message(filters.command("id"))
+async def idtest(client, message):
 
     try:
 
-        member = await client.get_chat_member(
-            FORCE_CHANNEL,
-            message.from_user.id
-        )
+        chat = await client.get_chat(FORCE_CHANNEL)
 
         await message.reply_text(
-            f"STATUS: {member.status}"
+            f"CHANNEL ID:\\n{chat.id}"
         )
 
     except Exception as e:
@@ -29,9 +36,6 @@ async def start(client, message):
         await message.reply_text(
             f"ERROR:\\n{e}"
         )
-        @app.on_message(filters.command("id"))
-async def idtest(client, message):
-    chat = await client.get_chat(FORCE_CHANNEL)
-    await message.reply_text(str(chat.id))
+
 
 app.run()
