f2d33c94938c1abedce6e83891ac6873e6e3d14a
iplayervudey-pixel <iplayervudey@gmail.com>
2026-05-10 21:25:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("BOT ONLINE")
+from pyrogram import Client, filters
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+app = Client(
+    "bot",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH
+)
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text("✅ Bot berhasil online")
+
+app.run()
